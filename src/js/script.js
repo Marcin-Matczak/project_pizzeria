b76219ae4527e0451f5254125de5dd4aaf25385b
Marcin-Matczak <marcorn.88@gmail.com>
2022-04-01 09:59:48 +0200

    Added support for images after click event

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -147,40 +148,52 @@
 
       // for every category (param)...
       for (let paramId in thisProduct.data.params) {
+
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        //console.log('parampamparams:', paramId, param);
 
         // for every option in this category
         for (let optionId in param.options) {
+
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          //console.log(optionId, option);
-          console.log('array?:', Array.isArray(formData[paramId]));
+          const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
 
-          // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
-            // check if the option is not default
+          // check if there is param with a name of paramId in formData and if it includes optionId         
+          if (optionSelected) {
+
+            // check if the option is not default          
             if (!option.default) {
+
               // add option price to price variable
               price += option.price;
             }
           } else {
+
             // check if the option is default
             if (option.default) {
+
               // reduce price variable
               price -= option.price;
             }
           }
 
+          if (optionImage) {
+            console.log('Image:', optionImage);
+            
+            if (optionSelected) {
+              optionImage.classList.add('active');              
+            }
+            if (!optionSelected) {
+              optionImage.classList.remove('active');
+            }
+          }
         }
-
       }
-
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
-
   }
 
   const app = {
@@ -211,7 +224,7 @@
       thisApp.initData();
       thisApp.initMenu();
     },
-  }
+  };
 
   app.init();
 }
