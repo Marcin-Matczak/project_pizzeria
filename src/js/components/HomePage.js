ee5bb5a48cae8e78137b9353ba89e0b71de22092
Marcin-Matczak <marcorn.88@gmail.com>
2022-08-24 12:37:27 +0200

    Adding a carousel with opinions to the homepage

--- src/js/components/HomePage.js
+++ src/js/components/HomePage.js
@@ -5,14 +5,30 @@ class HomePage{
     const thisHome = this;
 
     thisHome.render(element);
+    thisHome.flickity();
   }
 
   render(element){
-    const thisHome = this;
-    console.log(thisHome);
+    const thisHome = this;    
 
     const generatedHTML = templates.homePage(element);
-    element.innerHTML = generatedHTML;
+    thisHome.dom = {
+      wrapper: element
+    };
+    
+    thisHome.dom.wrapper.innerHTML = generatedHTML;
+  }
+
+  flickity(){
+    const thisHome = this;
+
+    let elem = document.querySelector('.main-carousel');
+    thisHome.flkty = new Flickity( elem, {  // eslint-disable-line
+    // options
+      cellAlign: 'left',
+      contain: true,
+      autoPlay: true
+    });
   }
 }
 
